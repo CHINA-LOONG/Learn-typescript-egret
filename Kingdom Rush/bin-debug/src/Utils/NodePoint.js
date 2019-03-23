62d84ad92dbb5ca944d7b02bb2096273ef7591e3
薛龙 <xuelong9009@qq.com>
2019-03-24 01:38:59 +0800

    [添加] 添加塔防游戏Demo

---
+++ Kingdom Rush/bin-debug/src/Utils/NodePoint.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * 节点类
+ * @author
+ *
+ */
+var NodePoint = (function () {
+    function NodePoint(x, y) {
+        this.walkable = true;
+        this.x = x;
+        this.y = y;
+    }
+    var __egretProto__ = NodePoint.prototype;
+    return NodePoint;
+})();
+NodePoint.prototype.__class__ = "NodePoint";
